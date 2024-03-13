ca50c4b096f51cf1368390e3f86a96cf6af78eaf
Guillaume-BigG <guillaume.beaudoin96@gmail.com>
2024-03-13 10:28:09 +0100

    added all files

---
+++ get_next_line_bonus.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbeaudoi <gbeaudoi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 14:03:50 by gbeaudoi          #+#    #+#             */
+/*   Updated: 2023/12/05 20:52:24 by gbeaudoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4
+# endif
+
+char	*ft_strchr(const char *str, int c);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(const char *str);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *src);
+char	*clean_stash(char *stash);
+char	*read_line(char *stash);
+char	*get_next_line(int fd);
+char	*fill_stash(int fd, char *stash);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbeaudoi <gbeaudoi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 14:04:27 by gbeaudoi          #+#    #+#             */
+/*   Updated: 2023/12/06 13:20:26 by gbeaudoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*ft_strchr(const char *str, int c)
+{
+	int		i;
+	char	*s;
+
+	s = (char *)str;
+	i = 0;
+	while (s[i] != (char)c)
+	{
+		if (s[i] == '\0')
+			return (0);
+		i++;
+	}
+	return (s + i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char			*join;
+	size_t			len_join;
+	unsigned int	i;
+	unsigned int	j;
+
+	if (!s1)
+		return (NULL);
+	len_join = ft_strlen(s1) + ft_strlen(s2) + 1;
+	join = malloc(sizeof(char) * len_join);
+	if (!join)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i])
+		join[i++] = s1[j++];
+	j = 0;
+	while (s2[j])
+		join[i++] = s2[j++];
+	join[i] = '\0';
+	free(s1);
+	return (join);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	i;
+	size_t	length;
+
+	if (!s)
+		return (NULL);
+	length = ft_strlen(s);
+	if (start > length)
+		len = 0;
+	else if (len > (length - start))
+		len = length - start;
+	sub = (char *)malloc((len + 1) * sizeof(char));
+	if (sub == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		sub[i] = s[start];
+		i++;
+		start++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *src)
+{
+	size_t	i;
+	char	*dup;
+
+	i = ft_strlen(src);
+	dup = (char *)malloc(sizeof(char) * (i + 1));
+	if (!dup)
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		dup[i] = src[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
